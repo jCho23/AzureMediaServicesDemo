f7512e220d0e121cd2d569896227b4e9e306e560
JuneCho23 <junecho.ucb@gmail.com>
2017-09-08 20:58:04 -0700

    added VideoURL

--- AzureMediaServicesDemo/Constants/MediaConstants.cs
+++ AzureMediaServicesDemo/Constants/MediaConstants.cs
@@ -1,10 +1,9 @@
 ﻿using System;
+
 namespace AzureMediaServicesDemo.Constants
 {
     public static class MediaConstants
     {
-        public MediaConstants()
-        {
-        }
-    }
+		public const string VideoUrl = @"https://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)";
+	}
 }
