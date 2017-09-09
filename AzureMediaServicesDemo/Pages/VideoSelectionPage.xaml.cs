95f3ed7231c1d771083c6cbb214ad7daea42bd94
JuneCho23 <junecho.ucb@gmail.com>
2017-09-08 21:02:40 -0700

    created VideoSelectionPage

---
+++ AzureMediaServicesDemo/Pages/VideoSelectionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AzureMediaServicesDemo.Pages
+{
+    public partial class VideoSelectionPage : ContentPage
+    {
+        public VideoSelectionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
